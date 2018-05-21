fa1fbedf8f552b7fe70f014b794eee7063263f6d
Sergio Kulyk <sergiokulyk98@gmail.com>
2018-05-21 23:56:05 +0300

    Test mcvard method.

--- team5/main.py
+++ team5/main.py
@@ -1,19 +1,28 @@
+import numpy as np
+
 from team5.lib5.Team5 import *
 
 
 # Виводимо результат методу
 def print_res(method_name="none", res=None):
     print()
-    print("-" * 50, "\n")
+    print("-" * 50)
     print(method_name)
-    print(res)
-    print("\n", "-" * 50)
     print()
+    print("x:   ", res[0])
+    print("f(x):", res[1])
+    print("iter:", res[2])
+    print("-" * 50)
 
 
 # Тестовий приклад
-def f(x):
+def f1(x):
     return (x[0] + 2 * x[1] - 7) ** 2 + (2 * x[0] + x[1] - 5) ** 2
+
+
+# Тестовий приклад
+def f2(x):
+    return 4. * (x[0] - 5) ** 2 + (x[1] - 6) ** 2
 
 
 # Створюємо обєкт класа Team5
@@ -25,8 +34,23 @@ s = 40
 d = 2
 
 swarm_parties_name = "Метод рою частинок"
-swarm_parties_res = team5.swarm_parties(s, d, f)
+swarm_parties_res = team5.swarm_parties(s, d, f1)
 
 print_res(swarm_parties_name, swarm_parties_res)
+
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# Тест методу Макварада
+
+x0 = np.array([8., 9.])
+eps = 10 ** (-20)
+u = 10 ** 2
+
+mcvard_name = "Метод Макварада"
+mcvard_res = team5.mcvard(x0, eps, u, f2)
+
+print_res(mcvard_name, mcvard_res)
 
 # ----------------------------------------------------------
