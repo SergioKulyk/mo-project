874946c3869a273042a6b6e66d6d53906a3c79ff
Sergio Kulyk <sergiokulyk98@gmail.com>
2018-05-22 14:31:49 +0300

    Refactor.

--- team5/main.py
+++ team5/main.py
@@ -1,17 +1,6 @@
 from team5.lib5.Team5 import *
 from team5.test.test_methods import *
-
-
-# Виводимо результат методу
-def print_res(method_name="none", res=None):
-    print()
-    print("-" * 50)
-    print(method_name)
-    print()
-    print("x:   ", res[0])
-    print("f(x):", res[1])
-    print("iter:", res[2])
-    print("-" * 50)
+from team5.utilities.Utilities import *
 
 gol_name = "Метод золотого перерізу"
 tox_name = "Метод хорд"
@@ -30,7 +19,7 @@ b = 4
 
 golden_section_res = team5.golden_section(a, b, f_gol_test)
 
-print_res(gol_name, golden_section_res)
+Utilities.print_res(gol_name, golden_section_res)
 # ----------------------------------------------------------
 
 # ----------------------------------------------------------
@@ -41,7 +30,7 @@ eps = 10 ** -4
 
 toxins_res = team5.toxins(a, b, f_tox_test, d_tox_test)
 
-print_res(tox_name, toxins_res)
+Utilities.print_res(tox_name, toxins_res)
 # ----------------------------------------------------------
 
 # ----------------------------------------------------------
@@ -50,25 +39,25 @@ x = np.array([1, 3])
 
 huca_jivsa_res = team5.huca_jivsa(x, f_huc_test)
 
-print_res(huc_name, huca_jivsa_res)
+Utilities.print_res(huc_name, huca_jivsa_res)
 # ----------------------------------------------------------
 
 # ----------------------------------------------------------
 # Тест методу найшвідшого градієнтного спуску
 x = np.array([0., 1.])
 
-faster_gradient_descent_res = team5.faster_gradient_descent(x, f_fgd_test)
+faster_gradient_descent_res = team5.faster_gradient_descent(x, f_fgd_test, g_fgd_test)
 
-print_res(fgd_name, faster_gradient_descent_res)
+Utilities.print_res(fgd_name, faster_gradient_descent_res)
 # ----------------------------------------------------------
 
 # ----------------------------------------------------------
 # Тест методу Макварада
 x0 = np.array([8., 9.])
 
-mcvard_res = team5.mcvard(x0, f_huc_test)
+mcvard_res = team5.mcvard(x0, f_huc_test, g_mcv_test, h_mcv_test)
 
-print_res(mcv_name, mcvard_res)
+Utilities.print_res(mcv_name, mcvard_res)
 # ----------------------------------------------------------
 
 # ----------------------------------------------------------
@@ -78,5 +67,5 @@ d = 2
 
 swarm_parties_res = team5.swarm_parties(s, d, f_swr_test)
 
-print_res(swr_name, swarm_parties_res)
+Utilities.print_res(swr_name, swarm_parties_res)
 # ----------------------------------------------------------
